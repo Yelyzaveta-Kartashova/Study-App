84356ed776a5e258f7e2f3577e090cca5075f845
Eliza <kartashovaelizaveta191@gmail.com>
2024-08-10 17:54:03 +0100

    modules and navbar

---
+++ app/controllers/topics_controller.rb
@@ -0,0 +1,65 @@
+class TopicsController < ApplicationController
+  before_action :set_topic, only: %i[ show edit update destroy ]
+
+  def index
+    @topics = Topic.all
+  end
+
+  def show
+  end
+
+  def new
+    @topic = Topic.new
+  end
+
+  def edit
+  end
+
+  def create
+    @topic = Topic.new(topic_params)
+
+    respond_to do |format|
+      if @topic.save
+        format.html { redirect_to topic_url(@topic), notice: "Topic was successfully created." }
+        format.json { render :show, status: :created, location: @topic }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @topic.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+
+  def update
+    respond_to do |format|
+      if @topic.update(topic_params)
+        format.html { redirect_to topic_url(@topic), notice: "Topic was successfully updated." }
+        format.json { render :show, status: :ok, location: @topic }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @topic.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+
+  def destroy
+    @topic.destroy!
+
+    respond_to do |format|
+      format.html { redirect_to topics_url, notice: "Topic was successfully destroyed." }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+
+    def set_topic
+      @topic = Topic.find(params[:id])
+    end
+
+
+    def topic_params
+      params.require(:topic).permit(:name)
+    end
+end
