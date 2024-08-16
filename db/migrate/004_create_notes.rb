544477b6761e4fe7f41d9e409601599090455448
Eliza <kartashovaelizaveta191@gmail.com>
2024-08-16 12:25:45 +0100

    Subjects and topics dependency; first try

---
+++ db/migrate/004_create_notes.rb
@@ -0,0 +1,12 @@
+class CreateNotes < ActiveRecord::Migration[6.0]
+  def change
+    create_table :notes do |t|
+      t.string :name
+      t.text :description
+      t.references :subject, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
+
