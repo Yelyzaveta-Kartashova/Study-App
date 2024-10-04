951732957fcad86a0c937c5fe63be419a15eb6c6
Eliza <kartashovaelizaveta191@gmail.com>
2024-10-04 17:27:01 +0100

    Added devise, log_out button, js for notifications

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :users
+  
   resources :subjects do
     resources :topics do
       resources :lessons
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 7) do
+ActiveRecord::Schema[7.1].define(version: 8) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -53,6 +53,18 @@ ActiveRecord::Schema[7.1].define(version: 7) do
     t.string "slug"
     t.index ["slug"], name: "index_topics_on_slug", unique: true
     t.index ["subject_id"], name: "index_topics_on_subject_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "lessons", "topics"
