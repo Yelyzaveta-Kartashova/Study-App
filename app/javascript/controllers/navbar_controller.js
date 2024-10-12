29895d12ae62986532dc984d36da0450acfc9ac1
Eliza <kartashovaelizaveta191@gmail.com>
2024-10-12 10:01:20 +0100

    added second level to navbar

--- app/javascript/controllers/navbar_controller.js
+++ app/javascript/controllers/navbar_controller.js
@@ -1,20 +1,37 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-  static targets = ["menu", "mobileMenu"];
 
-  toggle() {
-    this.menuTarget.classList.toggle("hidden");
+  toggleSubjectsMenu() {
+
+    const subjectsMenu = document.getElementById("subjects-menu");
+    subjectsMenu.classList.toggle("hidden");
+      
+    console.log('subjects menu');
   }
 
-  hide(event) {
-    if (!this.element.contains(event.target)) {
-      this.menuTarget.classList.add("hidden");
-    }
+  toggleTopicsMenu(event) {
+
+    event.preventDefault();
+    const topicsMenu = document.getElementById("topics-menu");
+    topicsMenu.classList.toggle("hidden");
+     
+    console.log('topics menu');
   }
 
   toggleMobileMenu() {
+
     const mobileMenu = document.getElementById("mobile-menu");
     mobileMenu.classList.toggle("hidden");
+    
+    console.log('mobile');
+  }
+
+  hello(e) {
+    console.log(`here: ${e.currentTarget.dataset.subject}`)
+    const topicLinks = document.getElementById(`subjects-topics-menu-${e.currentTarget.dataset.subject}`);
+    topicLinks.classList.toggle("hidden");
+    e.preventDefault();
+    console.log('hi');
   }
 }
