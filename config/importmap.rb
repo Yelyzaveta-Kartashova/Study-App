4fe6c98987acc46b59977da07e00f1f84883d82c
Eliza <kartashovaelizaveta191@gmail.com>
2024-12-14 19:19:13 +0000

    Importmap heroku fixes

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,10 +1,10 @@
 # Pin npm packages by running ./bin/importmap
 # 
 pin "application"
-pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
-pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
-pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
-pin "stimulus-textarea-autogrow" # @4.1.0
+# pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
+# pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
+# pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
+# pin "stimulus-textarea-autogrow" # @4.1.0
 
 pin "flowbite", to: "https://cdn.jsdelivr.net/npm/flowbite@2.5.2/dist/flowbite.turbo.min.js"
 
