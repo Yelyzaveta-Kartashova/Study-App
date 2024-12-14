67b33d9fa05719d0405e385b96fa9257b4a04e7d
Eliza <kartashovaelizaveta191@gmail.com>
2024-12-14 18:04:13 +0000

    Commented out welcome_email to seed users

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
 
-  after_create :send_welcome_email
+  #after_create :send_welcome_email
 
   def admin?
     self.roles.exists?(name: 'Admin')
