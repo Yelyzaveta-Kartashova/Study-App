7dad313a5ffb206b69ebfdf71881095dfcea7b5a
Eliza <kartashovaelizaveta191@gmail.com>
2024-12-14 19:16:09 +0000

    importmap fixes

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,10 +1,11 @@
 # Pin npm packages by running ./bin/importmap
 # 
 pin "application"
-pin "@hotwired/turbo-rails", to: "turbo.min.js"
-pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
+pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
+pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
+pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin "stimulus-textarea-autogrow" # @4.1.0
-pin "@hotwired/stimulus", to: "@hotwired--stimulus.js" # @3.2.2
+
 pin "flowbite", to: "https://cdn.jsdelivr.net/npm/flowbite@2.5.2/dist/flowbite.turbo.min.js"
 
 pin_all_from "app/javascript/controllers", under: "controllers"
@@ -15,3 +16,5 @@ pin "slim-select" # @2.9.0
 pin "trix"
 pin "@rails/actiontext", to: "actiontext.esm.js"
 pin "@stimulus-components/clipboard", to: "@stimulus-components--clipboard.js" # @5.0.0
+
+
