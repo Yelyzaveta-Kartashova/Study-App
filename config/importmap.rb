36744263398c47ef8b7ecc2a7c324b809835905e
Eliza <kartashovaelizaveta191@gmail.com>
2024-12-14 18:52:38 +0000

    Changes to importmap

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,10 +1,14 @@
 # Pin npm packages by running ./bin/importmap
 # 
 pin "application"
-pin "@hotwired/turbo-rails", to: "turbo.min.js"
+
+pin "turbo", to: "https://cdn.jsdelivr.net/npm/@hotwired/turbo@7.1.0"
+
+# pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin "stimulus-textarea-autogrow" # @4.1.0
 pin "@hotwired/stimulus", to: "@hotwired--stimulus.js" # @3.2.2
+
 pin "flowbite", to: "https://cdn.jsdelivr.net/npm/flowbite@2.5.2/dist/flowbite.turbo.min.js"
 
 pin_all_from "app/javascript/controllers", under: "controllers"
