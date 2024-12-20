0075c43fc25082f308c849431c6b5f5244a328f1
Eliza <kartashovaelizaveta191@gmail.com>
2024-12-20 14:34:13 +0000

    fixes for importmap

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,6 +2,6 @@ import "@hotwired/turbo-rails"
 import "controllers"
 // import "./custom/alerts"
  import "trix"        
-// import "@rails/actiontext"
- import 'flowbite'
+ import "@rails/actiontext"
+// import 'flowbite'
 import "./trix-editor-overrides"
