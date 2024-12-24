1ef84b7a29d8c65b4b4f6562e725adda0af53e27
Eliza <kartashovaelizaveta191@gmail.com>
2024-12-24 17:13:49 +0000

    Changed open ai controller

--- app/javascript/controllers/open_ai_controller.js
+++ app/javascript/controllers/open_ai_controller.js
@@ -12,33 +12,10 @@ export default class extends Controller {
     const showMore = document.getElementById("generated_content");
     const isCollapsed = showMore.classList.contains("line-clamp-4");
 
+
     showMore.classList.toggle("overflow-hidden");
     showMore.classList.toggle("line-clamp-4");
 
     this.buttonTextTarget.innerText = isCollapsed ? "Show less" : "Show more";
-  }
-
-  initialize() {
-    const content = document.getElementById("generated_content");
-
-    if (content.scrollHeight > content.clientHeight) {
-      this.showButton();
-    } else {
-      this.hideButton();
-    }
-  }
-
-  showButton() {
-    const button = this.element.querySelector("[data-open-ai-target='buttonText']").closest("div");
-    if (button) {
-      button.classList.remove("hidden"); 
-    }
-  }
-
-  hideButton() {
-    const button = this.element.querySelector("[data-open-ai-target='buttonText']").closest("div");
-    if (button) {
-      button.classList.add("hidden"); 
-    }
   }
 }
